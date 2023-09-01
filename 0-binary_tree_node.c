0cd2d264811d87b53e2c04c102ea14e836e13d22
Akuna Joshua <akunajoshua@gmail.com>
2023-09-01 02:27:19 +0100

    0-binary_tree_node.c : correction : betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - creates a binary tree node.
  * @parent: the parent node for the newly created node.
  * @value: the value of the n attrubute of the new created node of
- * 	type binary_tree_t.
+ *	type binary_tree_t.
  * Return: returns the newly created node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
